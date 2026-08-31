48ae342b54e238c52cc2d1653294fabda00e3efc
luwura <laura.maria2006@yahoo.com>
2026-08-31 21:44:08 +0300

    setup

---
+++ avs-project/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './NavBar'
+
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ avs-project/src/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './NavBar.css'
+const NavBar = () =>
+{
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
